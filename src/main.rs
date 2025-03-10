1b731e04d65f5289dfd9c3eb2bc2e9f558b30ddc
anomitroid <anomitroid2005@gmail.com>
2025-03-11 00:27:03 +0530

    function for parsing xml file almost complete, moving on to parsing directory

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
+use std::io;
 use std::fs::File;
-use std::process::exit;
 use xml::reader::{EventReader, XmlEvent};
+
+fn read_entire_xml_file(file_path: &str) -> io::Result<String> {
+    let file = File::open(file_path)?;
+    let er = EventReader::new(file);
+    let mut content = String::new();
+    for event in er.into_iter() {
+        if let XmlEvent::Characters(text) = event.expect("TODO") {
+            content.push_str(&text);
+        }
+    }
+    Ok(content)
+}
 
 fn main() {
     let file_path = "docs.gl/gl4/glClear.xhtml";
-    let file = File::open(file_path).unwrap_or_else(
-        |err| {
-            eprintln!("ERROR: could not read file '{}': {}", file_path, err);
-            exit(1);
-        }
-    ); 
-    let er = EventReader::new(file);
-    for event in er.into_iter() {
-        let event = event.unwrap_or_else(
-            |err| {
-                eprintln!("ERROR: could not read event: {}", err);
-                exit(1);
-            }
-        );
-        if let XmlEvent::Characters(text) = event {
-            println!("{}", text);
-        }
-    }
+    println!("{content}", content = read_entire_xml_file(file_path).expect("TODO"));
 }
