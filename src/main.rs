a5606d032ad2e24070b13e6564e28bd66d99889d
anomitroid <anomitroid2005@gmail.com>
2025-03-11 02:19:15 +0530

    condensed repetitive blocks of code

--- src/main.rs
+++ src/main.rs
@@ -20,32 +20,32 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    fn chop(&mut self, n: usize) -> &'a [char] {
+        let token = &self.content[..n];
+        self.content = &self.content[n..];
+        return token;
+    }
+
+    fn chop_while<P>(&mut self, mut predicate: P) -> &'a [char] where P: FnMut(&char) -> bool {
+        let mut n = 0;
+        while n < self.content.len() && predicate(&self.content[n]) {
+            n += 1;
+        }
+        self.chop(n)
+    }
+
     fn next_token(&mut self) -> Option<&'a [char]> {
         self.trim_left();
         if self.content.len() == 0 {
             return None;
         }
         if self.content[0].is_numeric() {
-            let mut n = 0;
-            while n < self.content.len() && self.content[n].is_numeric() {
-                n += 1;
-            }
-            let token = &self.content[..n];
-            self.content = &self.content[n..];
-            return Some(token);
+            return Some(self.chop_while(|c| c.is_numeric()));
         }
         if self.content[0].is_alphabetic() {
-            let mut n = 0;
-            while n < self.content.len() && self.content[n].is_alphabetic() {
-                n += 1;
-            }
-            let token = &self.content[..n];
-            self.content = &self.content[n..];
-            return Some(token);
+            return Some(self.chop_while(|c| c.is_alphabetic()));
         }
-        let token = &self.content[..1];
-        self.content = &self.content[1..];
-        Some(token)
+        return Some(self.chop(1));
     }
 }
 
@@ -80,7 +80,7 @@ fn main() -> io::Result<()> {
         .chars()
         .collect::<Vec<char>>();    
     for token in Lexer::new(&content) {
-        println!("{token}", token = token.iter().collect::<String>());
+        println!("{token}", token = token.iter().map(|x| x.to_ascii_uppercase()).collect::<String>());
     }
     // let all_documents = HashMap::<Path, HashMap<String, usize>>::new();
     // let dir_path = "docs.gl/gl4/";
