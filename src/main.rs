e8c433403708f9a9c8de7bb7297d80018a2203a7
anomitroid <anomitroid2005@gmail.com>
2025-03-11 00:18:41 +0530

    now we can get just the text out of the xhtml without the tags

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::process::exit;
-use xml::reader::EventReader;
+use xml::reader::{EventReader, XmlEvent};
 
 fn main() {
     let file_path = "docs.gl/gl4/glClear.xhtml";
@@ -12,6 +12,14 @@ fn main() {
     ); 
     let er = EventReader::new(file);
     for event in er.into_iter() {
-        println!("{:?}", event);
+        let event = event.unwrap_or_else(
+            |err| {
+                eprintln!("ERROR: could not read event: {}", err);
+                exit(1);
+            }
+        );
+        if let XmlEvent::Characters(text) = event {
+            println!("{}", text);
+        }
     }
 }
