6ed943fd67d4da0fae37fda51a758a9006e9874f
anomitroid <anomitroid2005@gmail.com>
2025-03-19 20:10:24 +0530

    serving js as well as html

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::collections::HashMap;
 use std::env;
 use std::process::{exit, ExitCode};
 use std::result::Result;
-use tiny_http::{Header, Request, Response, Server};
+use tiny_http::{Header, Method, Request, Response, Server, StatusCode};
 
 struct Lexer<'a> {
     content: &'a [char],
@@ -152,15 +152,35 @@ fn usage(program: &str) {
 
 fn serve_request(request: Request) -> Result<(), ()> {
     println!("INFO: Received request! method: {:?}, url: {:?}", request.method(), request.url());
-    let content_type_text_html = Header::from_bytes("Content-Type", "text/html; charset=utf-8").expect("header is fine");
-    let index_html_path = "index.html";
-    let index_html_file = File::open(index_html_path).map_err(|err| {
-        eprintln!("ERROR: could not open {index_html_path}: {err}", index_html_path = index_html_path, err = err);
-    })?;
-    let response = Response::from_file(index_html_file).with_header(content_type_text_html);
-    request.respond(response).map_err(|err| {
-        eprintln!("ERROR: could not respond to request: {err}", err = err);
-    })?;
+    match (request.method(), request.url()) {
+        (Method::Get, "/index.js") => {
+            let content_type_text_javascript = Header::from_bytes("Content-Type", "text/javascript; charset=utf-8").expect("header is fine");
+            let index_js_path = "index.js";
+            let index_js_file = File::open(index_js_path).map_err(|err| {
+                eprintln!("ERROR: could not open {index_js_path}: {err}", index_js_path = index_js_path, err = err);
+            })?;
+            let response = Response::from_file(index_js_file).with_header(content_type_text_javascript);
+            request.respond(response).map_err(|err| {
+                eprintln!("ERROR: could not respond to request: {err}", err = err);
+            })?;        
+        }
+        (Method::Get, "/") | (Method::Get, "/index.html") => {
+            let content_type_text_html = Header::from_bytes("Content-Type", "text/html; charset=utf-8").expect("header is fine");
+            let index_html_path = "index.html";
+            let index_html_file = File::open(index_html_path).map_err(|err| {
+                eprintln!("ERROR: could not open {index_html_path}: {err}", index_html_path = index_html_path, err = err);
+            })?;
+            let response = Response::from_file(index_html_file).with_header(content_type_text_html);
+            request.respond(response).map_err(|err| {
+                eprintln!("ERROR: could not respond to request: {err}", err = err);
+            })?;        
+        },
+        _ => {
+            request.respond(Response::from_string("404").with_status_code(StatusCode(404))).map_err(|err| {
+                eprintln!("ERROR: could not respond to request: {err}", err = err);
+            })?;
+        }
+    }
     Ok(())
 } 
 
