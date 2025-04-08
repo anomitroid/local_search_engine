18c1e3dc2293436a6ed8b6589ca853601be7b753
anomitroid <anomitroid2005@gmail.com>
2025-04-08 14:23:57 +0530

    removed nan source

--- src/model.rs
+++ src/model.rs
@@ -405,9 +405,12 @@ impl Model for InMemoryModel {
             for token in &tokens {
                 rank += compute_tf(&token, doc) * compute_idf(&token, self.docs.len(), &self.df);
             }
-            result.push((path.clone(), rank));
+            if !rank.is_nan() {
+                result.push((path.clone(), rank));
+            }
         }
-        result.sort_by(|(_, rank1), (_, rank2)| rank1.partial_cmp(rank2).unwrap().reverse());
+        result.sort_by(|(_, rank1), (_, rank2)| rank1.partial_cmp(rank2).expect(&format!("{rank1} and {rank2} are not comparable")));
+        result.reverse();
         Ok(result)
     }
 
